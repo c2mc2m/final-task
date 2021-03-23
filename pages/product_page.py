fa8c18ccb280fc9a568e15308825b8b0babb1792
c2m <camamileup@gmail.com>
2021-03-23 16:29:59 +0200

    add unsuccessful verifications

--- pages/product_page.py
+++ pages/product_page.py
@@ -4,7 +4,7 @@ from .locators import ProductPageLocator
 
 class ProductPage(BasePage):
     def should_add_to_basket(self):
-        self.should_be_promo_url()
+        #self.should_be_promo_url()
         self.should_be_button_add_to_basket()
         self.go_to_basket()
 
@@ -15,24 +15,39 @@ class ProductPage(BasePage):
         self.is_added_itemname_equal_to_catalogname()
 
     def should_be_promo_url(self):
-        assert self.is_required_url("?promo="), "The current url is not 'promo' url"
+        assert self.is_required_url("?promo="), \
+            "The current url is not 'promo' url"
     
     def should_be_button_add_to_basket(self):
-        assert self.is_element_present(*ProductPageLocator.ADD_TO_BASKET_BUTTON), "The ad_to_basket button is absent"
+        assert self.is_element_present(*ProductPageLocator.ADD_TO_BASKET_BUTTON), \
+            "The ad_to_basket button is absent"
 
     def go_to_basket(self):
         self.busket_link = self.browser.find_element(*ProductPageLocator.ADD_TO_BASKET_BUTTON)
         self.busket_link.click()
 
     def should_see_added_msg(self):
-        assert self.is_element_present(*ProductPageLocator.PRODUCT_ADDED_MSG), "There is no 'added item' message"
+        assert self.is_element_present(*ProductPageLocator.PRODUCT_ADDED_MSG), \
+            "There is no 'added item' message"
 
     def should_see_cost_msg(self):
-        assert self.is_element_present(*ProductPageLocator.BASKET_TOTAL_COST_MSG), "There is no info-message about the cost of the items in the basket"
+        assert self.is_element_present(*ProductPageLocator.BASKET_TOTAL_COST_MSG), \
+            "There is no info-message about the cost of the items in the basket"
 
     def is_added_cost_equal_to_price(self):
-        assert self.value_compare(ProductPageLocator.ITEM_PRICE, ProductPageLocator.ADDED_TO_BASKET_PRICE), "The prices are not equal"
+        assert self.value_compare(ProductPageLocator.ITEM_PRICE, ProductPageLocator.ADDED_TO_BASKET_PRICE), \
+            "The prices are not equal"
     
     def is_added_itemname_equal_to_catalogname(self):
-        assert self.value_compare(ProductPageLocator.ITEM_NAME, ProductPageLocator.ADDED_TO_BASKET_ITEM_NAME), "The items name are not equal"
+        assert self.value_compare(ProductPageLocator.ITEM_NAME, ProductPageLocator.ADDED_TO_BASKET_ITEM_NAME), \
+            "The items name are not equal"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocator.PRODUCT_ADDED_MSG), \
+            "Success message is presented, but should not be"
+
+    def should_not_disappear(self):
+        assert self.is_disappeared(*ProductPageLocator.PRODUCT_ADDED_MSG), \
+            "The message is disappeared, but shouldt be"
+
         
--- test_product_page.py
+++ test_product_page.py
@@ -9,7 +9,7 @@ import pytest
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
                      pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",\
-                     marks=pytest.mark.xfail),
+                                   marks=pytest.mark.xfail),
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 def test_guest_can_add_product_to_basket(browser, link):
@@ -19,4 +19,22 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.solve_quiz_and_get_code()
     page.should_see_info_about_adding()
 
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    page.open()
+    page.should_add_to_basket()
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    page.open()
+    page.should_not_be_success_message()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    page.open()
+    page.should_add_to_basket()
+    page.should_not_disappear()
 
